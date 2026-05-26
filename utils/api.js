3abcebcfa9a0aa67c50832a5c143eee58be44c26
dxnah <caburatandinah@gmail.com>
2026-05-26 18:22:58 +0800

    mobile deployment

--- utils/api.js
+++ utils/api.js
@@ -1,3 +1,5 @@
-const BASE_URL = 'http://192.168.0.104:8000/api';
+import Constants from 'expo-constants';
+
+const BASE_URL = Constants.expoConfig?.extra?.apiUrl ?? 'http://10.194.32.75:8000/api';
 
 export default BASE_URL;
